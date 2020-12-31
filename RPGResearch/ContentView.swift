59f7abd8bf4f1a9581b3e52c230d5c817543ce13
00557001 <00557001@mail.ntou.edu.tw>
2020-12-31 14:21:39 +0800

    Initial Commit

---
+++ RPGResearch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RPGResearch
+//
+//  Created by User10 on 2020/12/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RPGResearch/RPGResearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  RPGResearchApp.swift
+//  RPGResearch
+//
+//  Created by User10 on 2020/12/31.
+//
+
+import SwiftUI
+
+@main
+struct RPGResearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
